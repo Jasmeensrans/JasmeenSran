524c3f933c313aa71f1d2bcd359c4dd6e1519d73
Jasmeen Sran <jasmeensran@hotmail.co.uk>
2023-12-24 13:57:50 -0500

    Updates

---
+++ precache-manifest.6e96208ebcd364e04bcc3610e17c5169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e971be1144064b28327278d8a14257",
+    "url": "/JasmeenSran/index.html"
+  },
+  {
+    "revision": "f897bd6e9e22d34cb5c0",
+    "url": "/JasmeenSran/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "051cbbd80c56936e65d7",
+    "url": "/JasmeenSran/static/js/2.722e2055.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/JasmeenSran/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "f897bd6e9e22d34cb5c0",
+    "url": "/JasmeenSran/static/js/main.0be4335f.chunk.js"
+  },
+  {
+    "revision": "8d13bca15ab7aeb47ab7",
+    "url": "/JasmeenSran/static/js/runtime~main.ab3fb5d9.js"
+  }
+]);
